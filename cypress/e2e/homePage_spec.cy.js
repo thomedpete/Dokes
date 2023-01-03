4bc98143660b851b449b742a511c4ece7ea30e84
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-01-03 11:22:38 -0700

    Adds the cypress install and 1 spec sheet for the home page

---
+++ cypress/e2e/homePage_spec.cy.js
@@ -0,0 +1,18 @@
+describe('onLoad', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Should see the title of the page', () => {
+    cy.get('')
+  });
+
+  it('Should see a random joke displayed ', () => {
+    cy.get('')
+  });
+
+  it('', () => {
+  });
+
+
+})
