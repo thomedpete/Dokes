258903af21b0005d496e944fb10fb4277cd69f52
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-01-02 19:08:48 -0700

    Adds button

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 import Header from '../Header/Header';
 import HeroBanner from '../HeroBanner/HeroBanner';
+import JokeCard from '../JokeCard/JokeCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <h1>LameWire</h1>
       <HeroBanner/>
+      <JokeCard/>
+      <Button variant="contained">New Joke</Button>
     </div>
   );
 }
