82cf76e7edd7688692589489054c9fafbbe60587
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-01-04 16:11:41 -0600

    Fixed banner and add functionality to the button

--- src/Components/JokeCard/JokeCard.js
+++ src/Components/JokeCard/JokeCard.js
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import "./JokeCard.css"
