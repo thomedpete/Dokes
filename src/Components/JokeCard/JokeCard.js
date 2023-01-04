62c53e8d59e5c54d7f00df490fb5cbfed017e574
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-01-04 11:20:13 -0700

    Adds a favorite checkHeart to the joke display card

--- src/Components/JokeCard/JokeCard.js
+++ src/Components/JokeCard/JokeCard.js
@@ -4,29 +4,30 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import "./JokeCard.css"
 import { Stack } from '@mui/material';
 
+const label = { inputProps: { 'aria-label': 'Favorite button to put in pocket/saved jokes' } }
+
 
 const JokeCard = ({ jokes }) => {
+  const [checked, setChecked] = React.useState(true);
+  
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
   return (
     <div className='card-container'>
 
       <Card sx={{ maxWidth: '60%', backgroundColor: "lightsteelblue" }}>
-        {/* <CardMedia
-
-          sx={{ height: 100 }}
-          image=""
-          title=""
-
-        />
-        
-
-        /> */}
-
         <Stack
           direction="column"
           spacing={2}
@@ -34,9 +35,8 @@ const JokeCard = ({ jokes }) => {
           alignItems="center"
         >
           <CardContent flexGrow="1">
-     
-
             <Card sx={{ backgroundColor: "#1976d2" }}>
+              <Checkbox {...label} checked={checked} onChange={handleChange} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
               <Typography m="2.55%" gutterBottom variant="h5" color="gainsboro">
                 {jokes}
               </Typography>
