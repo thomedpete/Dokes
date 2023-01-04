14a945839fc01c2ffc984f500c7b71b8b11d0a64
Matt Walter <matt.walter.tx@outlook.com>
2023-01-04 17:30:44 -0600

    Add about component

---
+++ src/Components/About/About.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
+
+const About = () => {
+    return (
+      <div>
+        <Typography>
+          About Us!
+        </Typography>
+        <Typography>
+          Flotsam jestsam dad joke har har har
+        </Typography>
+        </div>
+    )
+  }
+
+
+export default About;
