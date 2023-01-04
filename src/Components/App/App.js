c432534a6ce30e59532148394ea679a2a2dd09ac
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-01-04 15:17:19 -0700

    Adds a blank theme template for our color pallet to be filled

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,13 +9,16 @@ import getAllJokes from '../../apiCalls'
 import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
+import { ThemeProvider } from '@mui/material';
 import banner from '../../assets/banner2.png'
+import theme from '../../theme';
 // import getAllJokes from '../../apiCalls'
 import './App.css';
 
 const label = { inputProps: { 'aria-label': 'Favorite button to put in pocket/saved jokes' } }
 
 const App = () => {
+
   const [checked, setChecked] = useState(true);
   const handleChange = (event) => {
     setChecked(event.target.checked);
@@ -45,26 +48,33 @@ const App = () => {
   }, [])
 
   return (
-    <div className="App">
-      <Header />
-      <img src={banner} className='banner' />
-      <Stack
-        direction="column"
-        spacing={2}
-        justifyContent="space-between"
-        alignItems="center"
-        margin={2}
-      >
-        <Button variant="contained" sx={{ fontSize: "150%", width: "35%", fontWeight: "bold"  }}>POOF! You're a Sandwich</Button>
-      <HeroBanner/>
-      <JokeCard jokes={jokes} />
-        <Checkbox {...label} checked={checked} onChange={handleChange} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
-      <Button variant="contained">New Joke</Button>
-      {/* we need to pass pocket down to Pocket, and set Pockets state to include the checked joke and any jokes that were alrady a part of that state */}
-      </Stack>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+        <img src={banner} className='banner' />
+        <Stack
+          direction="column"
+          spacing={2}
+          justifyContent="space-between"
+          alignItems="center"
+          margin={2}
+        >
 
 
-    </div>
+          <Button variant="contained" sx={(theme) => ({
+            fontSize: "150%",
+            width: "35%",
+            fontWeight: "bold",
+            bgcolor: 'secondary.main'
+          })}>POOF! You're a Sandwich</Button>
+          <HeroBanner />
+          <JokeCard jokes={jokes} />
+          <Checkbox {...label} checked={checked} onChange={handleChange} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+          <Button variant="contained" >New Joke</Button>
+          {/* we need to pass pocket down to Pocket, and set Pockets state to include the checked joke and any jokes that were alrady a part of that state */}
+        </Stack>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+
+    primary: {
+      main: '',
+      light: '',
+      dark: '',
+      contrastText: ''
+    },
+
+    secondary: {
+      main: '',
+      light: '',
+      dark: '',
+      contrastText: ''
+    },
+
+    error: {
+      main: '',
+      light: '',
+      dark: '',
+      contrastText: ''
+    },
+
+    warning: {
+      main: '',
+      light: '',
+      dark: '',
+      contrastText: ''
+    },
+    info: {
+      main: '',
+      light: '',
+      dark: '',
+      contrastText: ''
+    },
+    success: {
+      main: '',
+      light: '',
+      dark: '',
+      contrastText: ''
+    },
+  }
+
+})
+
+export default theme
