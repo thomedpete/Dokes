ecc4572640176def5a81e95c5c57a3346de0a786
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-01-03 17:46:23 -0700

    Adds as many cypress tests as I can make so far

--- cypress/e2e/homePage_spec.cy.js
+++ cypress/e2e/homePage_spec.cy.js
@@ -22,5 +22,19 @@ describe('onLoad', () => {
   it.skip('', () => {
   });
 
+  it.skip('Should fetch from  correct api url', () => {
+    cy.intercept(`dadjokeAPIurl`, {
+      jokes:
+      {
+        average_rating: 4,
+        backdrop_path: "https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+        id: 436270,
+        poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+        release_date: "2022-10-19",
+        title: "Black Adam"
+      },
+    })
+  });
+
 
 })
