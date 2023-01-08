28b81863c200333f89896bc34619fed5d78f2e78
Thomas Peterson <thomasedwardpeterson@gmail.com>
2023-01-08 14:30:14 -0700

    Adds another test for the redirect to home button

--- cypress/e2e/errorPage.cy.js
+++ cypress/e2e/errorPage.cy.js
@@ -4,8 +4,13 @@ describe('Visiting the About Page', () => {
   })
 
   it('user should see an error page when they go to the wrong url', () => {
-    cy.get('.aboutUs-title').contains('About Dokes')
+    cy.get('.App > :nth-child(2)').should('exist')
+  })
+
+  it('User should be able to navigate home using the home button', () => {
+    cy.get('a > button').click()
+    cy.url().should('eq','http://localhost:3000/')
   })
   
 
-})
+}) 
