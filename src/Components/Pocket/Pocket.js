96fd5d357df5f6683bfec9cb5c4c8faf705d8896
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-01-08 14:25:14 -0600

    Change on pocket

--- src/Components/Pocket/Pocket.js
+++ src/Components/Pocket/Pocket.js
@@ -16,8 +16,6 @@ import { gsap, Power3 } from 'gsap'
 import './Pocket.css'
 
 
-// const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 const theme = createTheme();
 
 const Pocket = ({ pocket, deleteJoke }) => {
