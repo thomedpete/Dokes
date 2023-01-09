174f19887dfb4f6ca7a7ac6afee11b272c4be78f
Matt Walter <matt.walter.tx@outlook.com>
2023-01-09 14:40:43 -0600

    Add styling to error page

--- src/Components/BadURL/BadURL.js
+++ src/Components/BadURL/BadURL.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import '../About/About.css'
 
 
 const BadURL = () => {
     return (
-        <div>
-            <h2>on the wrong page dodo!</h2>
-            <h3>go back home</h3>
-            <Link to='/'><button>home</button></Link>
+        <div className='about-container'>
+            <article className='aboutUs-container'>
+                <h2 className='about-dokes'>on the wrong page dodo!</h2>
+                <h3 className='about-dokes'>go back home</h3>
+                <Link to='/'><button>home</button></Link>
+            </article>
         </div>
     )
 }
